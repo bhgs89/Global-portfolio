3f6364b3f08a44dc06a07065972de1b21dcd5552
bhgs89 <han.baek.program@gmail.com>
2019-12-12 16:51:02 -0800

    Basic styling of Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,33 +5,35 @@ class Home extends React.Component {
   render() {
     return (
       <section className="home-section">
-        <div className="container home-main">
-          <div className="row">
-            <div className="col-md-5">
-              <h1>Your next interactive experience</h1>
-              <button>Hire us</button>
-            </div>
-            <div className="col-md-7">
-              <img src="/images/introduction-visual.png" alt="" />
-            </div>
+        <div className="home-main">
+          <div>
+            <h1>Your next interactive experience</h1>
+            <button className="home-hire-button">
+              <div className="button-background"></div>
+              <span>Hire us &#x2192;</span>
+            </button>
+          </div>
+          <div>
+            <img src="/images/introduction-visual.png" alt="" />
           </div>
         </div>
-        <div className="container">
+
+        <div className="container description">
           <div className="row">
-            <div className="col-md-4">
-              <h4>Metiew and Smith</h4>
+            <div className="col-md-4 desc-item">
+              <h3>Metiew &#x26; Smith</h3>
               <p>
                 Lorem ipsum dolor sit amet. consectetur adipisicing elit sed do
               </p>
             </div>
-            <div className="col-md-4">
-              <h4>Metiew and Smith</h4>
+            <div className="col-md-4 desc-item">
+              <h3>Fantasy Interactive</h3>
               <p>
                 Lorem ipsum dolor sit amet. consectetur adipisicing elit sed do
               </p>
             </div>
-            <div className="col-md-4">
-              <h4>Metiew and Smith</h4>
+            <div className="col-md-4 desc-item">
+              <h3>Paul &#x26; Shark</h3>
               <p>
                 Lorem ipsum dolor sit amet. consectetur adipisicing elit sed do
               </p>
